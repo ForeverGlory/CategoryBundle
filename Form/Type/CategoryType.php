52ccc7451e5ded2c42057c0b59ef2ab5a0de03ff
ForeverGlory <foreverglory@qq.com>
2016-05-15 17:26:05 +0800

    CategoryType

---
+++ Form/Type/CategoryType.php
@@ -0,0 +1,37 @@
+<?php
+
+/*
+ * This file is part of the current project.
+ * 
+ * (c) ForeverGlory <http://foreverglory.me/>
+ * 
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Glory\Bundle\CategoryBundle\Form\Type;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilder;
+
+/**
+ * Description of ContentType
+ *
+ * @author ForeverGlory <foreverglory@qq.com>
+ */
+class CategoryType extends AbstractType
+{
+
+    public function buildForm(FormBuilder $builder, array $options)
+    {
+        $builder
+                ->add('name')
+        ;
+    }
+
+    public function getName()
+    {
+        return 'category';
+    }
+
+}
