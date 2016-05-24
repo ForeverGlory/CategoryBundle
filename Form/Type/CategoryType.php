ee205903014bc5b07806fed7221982d107cda368
ForeverGlory <foreverglory@qq.com>
2016-05-24 23:14:49 +0800

    作为目录选择字段

--- Form/Type/CategoryType.php
+++ Form/Type/CategoryType.php
@@ -12,21 +12,19 @@
 namespace Glory\Bundle\CategoryBundle\Form\Type;
 
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\FormBuilder;
+use Symfony\Component\Form\FormBuilderInterface;
 
 /**
- * Description of ContentType
+ * Description of CategoryType
  *
  * @author ForeverGlory <foreverglory@qq.com>
  */
 class CategoryType extends AbstractType
 {
 
-    public function buildForm(FormBuilder $builder, array $options)
+    public function buildForm(FormBuilderInterface $builder, array $options)
     {
-        $builder
-                ->add('name')
-        ;
+        
     }
 
     public function getName()
